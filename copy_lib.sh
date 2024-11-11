94a13134ea125093401d94507f8c81259f7424e5
yoonsangpark <yoonsangpark@gmail.com>
2024-11-11 23:49:49 +0900

    add libnl

--- copy_lib.sh
+++ copy_lib.sh
@@ -17,4 +17,9 @@ cp -vrf ~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/code/lib/external/__insta
 cp -vrf ~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/code/lib/external/__install/lib/libopencv_imgcodecs.so*	~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/BSP/root-fs/rootfs/usr/lib
 cp -vrf ~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/code/lib/external/__install/lib/libopencv_videoio.so*	~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/BSP/root-fs/rootfs/usr/lib
 
+echo "3rd libnl-3.so" 
+cp -vrf ~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/code/lib/external/__install/lib/libnl-3.so*	~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/BSP/root-fs/rootfs/usr/lib
+cp -vrf ~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/code/lib/external/__install/lib/libnl-genl-3.so*	~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/BSP/root-fs/rootfs/usr/lib
+#cp -vrf ~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/code/application/external/__install/bin/wpa_supplicant	~/ws/nt9856x_linux_sdk_release_glibc_v1.01.007/BSP/root-fs/rootfs/usr/bin
+
 echo "Copying Libs Done"
