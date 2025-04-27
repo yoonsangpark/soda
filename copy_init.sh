8473e4b23698a8dcdb8abbdcf0c3f7d69c84944a
yoonsangpark <yoonsangpark@gmail.com>
2025-04-27 11:05:44 +0900

    udhcpc backgroud

--- copy_init.sh
+++ copy_init.sh
@@ -3,7 +3,7 @@
 echo "Copying init SH..."
 
 
-#cp -rfv  S25_Net ../BSP/root-fs/rootfs/etc_Model/etc_562_IPCAM1_EVB/init.d/S25_Net
+cp -rfv  S25_Net ../BSP/root-fs/rootfs/etc_Model/etc_562_IPCAM1_EVB/init.d/S25_Net
 cp -rfv  S99_Sysctl ../BSP/root-fs/rootfs/etc_Model/etc_562_IPCAM1_EVB/init.d/S99_Sysctl
 
 
